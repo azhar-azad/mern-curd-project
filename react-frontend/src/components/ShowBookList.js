4479c934d9778cbc8e20025f0adaf88272b55d4a
azhar_azad <azhar.u.azad@gmail.com>
2022-01-06 01:37:49 +0600

    Component to show all books defined

---
+++ react-frontend/src/components/ShowBookList.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import axios from "axios";
+import '../App.css';
+import { Link } from "react-router-dom";
+import BookCard from './BookCard';
+
+class ShowBookList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:5000/api/books')
+            .then(res => {
+                this.setState({ books: res.data });
+            })
+            .catch(err => {
+                console.log('Error from ShowBookList');
+            });
+    }
+
+    render() {
+        const books = this.state.books;
+        console.log('PrintBook: ' + books);
+
+        let bookList;
+        if (!books) {
+            bookList = 'There is no book record!';
+        } else {
+            bookList = books.map((book, key) => <BookCard book={book} key={key} />);
+        }
+
+        return (
+            <div className="ShowBookList">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <br />
+                            <h2 className="display-4 text-center">Books List</h2>
+                        </div>
+
+                        <div className="col-md-11">
+                            <Link to="/create-book" className="btn btn-outline-warning float-right">
+                                + Add New Book
+                            </Link>
+                            <br />
+                            <br />
+                            <hr />
+                        </div>
+                    </div>
+
+                    <div className="list">
+                        {bookList}
+                    </div>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default ShowBookList;
