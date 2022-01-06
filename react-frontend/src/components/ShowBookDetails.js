807a0e8b2df5fa7f8af05633cc9babac3b1f6084
azhar_azad <azhar.u.azad@gmail.com>
2022-01-06 21:37:41 +0600

    Component defined to show a book's info

---
+++ react-frontend/src/components/ShowBookDetails.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from "axios";
+
+class ShowBookDetails extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            book: {}
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get(`http://localhost:5000/api/books/${this.props.match.params.id}`)
+            .then(res => {
+                this.setState({
+                    book: res.data
+                });
+            })
+            .catch(err => {
+                console.log('Error from ShowBookDetails');
+            });
+    }
+
+    onDeleteClick(id) {
+        axios
+            .delete(`http://localhost:5000/api/books/${id}`)
+            .then(res => {
+                this.props.history.push('/');
+            })
+            .catch(err => {
+                console.log('Error from ShowBookDetails_deleteClick');
+            });
+    }
+
+    render() {
+        const book = this.state.book;
+        let BookItem = <div>
+            <table className="table table-hober table-dark">
+                {/*
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                */}
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Title</td>
+                        <td>{book.title}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Author</td>
+                        <td>{book.author}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>ISBN</td>
+                        <td>{book.isbn}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Publisher</td>
+                        <td>{book.publisher}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Published Date</td>
+                        <td>{book.published_date}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>Description</td>
+                        <td>{book.description}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        return (
+            <div class="ShowBookDetails">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 m-auto">
+                            <br />
+                            <br />
+                            <Link to="/" class="btn btn-outline-warning float-left">
+                                Show Book List
+                            </Link>
+                        </div>
+                        <br />
+                        <div class="col-md-8 m-auto">
+                            <h1 class="display-4 text-center">
+                                Book's Record
+                            </h1>
+                            <p class="lead text-center">
+                                View Book's Info
+                            </p>
+                            <hr />
+                            <br />
+                        </div>
+                    </div>
+                    <div>
+                        { BookItem }
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <button type="button" class="btn btn-outline-danger btn-lg btn-block"
+                                onClick={this.onDeleteClick.bind(this, book._id)}>
+                                Delete Book
+                            </button>
+                            <br />
+                        </div>
+
+                        <div class="col-md-6">
+                            <Link to={`/edit-book/${book._id}`} class="btn btn-outline-info btn-lg btn-block">
+                                Edit Book
+                            </Link>
+                            <br />
+                        </div>
+                    </div>
+                    {/*
+                    <br />
+                    <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Book</button>
+                    <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Book</button>
+                    */}
+                </div>
+            </div>
+        );
+    }
+};
+
+export default ShowBookDetails;
